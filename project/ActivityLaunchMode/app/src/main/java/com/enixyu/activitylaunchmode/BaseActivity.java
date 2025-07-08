3ae638988c87dfb5af4af27b90b1d021427c5071
Enix Yu <enix223@163.com>
2025-07-08 08:58:43 +0800

    fix: navigation failed issue

--- project/ActivityLaunchMode/app/src/main/java/com/enixyu/activitylaunchmode/BaseActivity.java
+++ project/ActivityLaunchMode/app/src/main/java/com/enixyu/activitylaunchmode/BaseActivity.java
@@ -1,12 +1,18 @@
 package com.enixyu.activitylaunchmode;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity implements OnClickListener {
@@ -23,6 +29,17 @@ public abstract class BaseActivity extends AppCompatActivity implements OnClickL
     log("onCreate");
     setContentView(R.layout.activity_launch_mode);
 
+    Window window = getWindow();
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    window.setStatusBarColor(Color.rgb(0, 0, 0));
+
+    // 配置导航栏
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null && !isTaskRoot()) {
+      actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    // 注册按钮的点击事件
     Button btn1 = findViewById(R.id.btn1);
     btn1.setOnClickListener(this);
 
@@ -75,6 +92,11 @@ public abstract class BaseActivity extends AppCompatActivity implements OnClickL
     log("onDestroy");
   }
 
+  /**
+   * 处理点击事件
+   *
+   * @param view 触发点击事件的视图
+   */
   @Override
   public void onClick(View view) {
     Class<?> targetCls = null;
@@ -91,5 +113,20 @@ public abstract class BaseActivity extends AppCompatActivity implements OnClickL
     }
     Intent intent = new Intent(this, targetCls);
     startActivity(intent);
+  }
+
+  /**
+   * 响应导航栏菜单按钮点击事件
+   *
+   * @param item 导航栏菜单
+   * @return 是否以处理
+   */
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    if (item.getItemId() == android.R.id.home) {
+      finish();
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
--- project/ActivityLaunchMode/app/src/main/java/com/enixyu/activitylaunchmode/SingleTaskActivity.java
+++ project/ActivityLaunchMode/app/src/main/java/com/enixyu/activitylaunchmode/SingleTaskActivity.java
@@ -9,8 +9,6 @@ public class SingleTaskActivity extends BaseActivity {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_launch_mode);
-
     TextView label = findViewById(R.id.label);
     label.setText(R.string.activity_single_task_label);
   }
