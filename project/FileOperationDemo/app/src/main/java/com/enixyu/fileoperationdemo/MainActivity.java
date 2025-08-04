33dfbadfe561e00b34f5293a6c8db75eeb309bfd
Enix Yu <enix223@163.com>
2025-08-04 23:57:11 +0800

    feat: add data provider init

--- project/FileOperationDemo/app/src/main/java/com/enixyu/fileoperationdemo/MainActivity.java
+++ project/FileOperationDemo/app/src/main/java/com/enixyu/fileoperationdemo/MainActivity.java
@@ -3,12 +3,24 @@ package com.enixyu.fileoperationdemo;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.enixyu.fileoperationdemo.db.DataProvider;
+import com.enixyu.fileoperationdemo.db.FileDataProvider;
+import com.enixyu.fileoperationdemo.db.SharedPrefDataProvider;
+import com.enixyu.fileoperationdemo.db.SqliteDataProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+  private SqliteDataProvider mSqliteDataProvider;
+  private SharedPrefDataProvider mSharedPrefDataProvider;
+  private FileDataProvider mFileDataProvider;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mSqliteDataProvider = new SqliteDataProvider(this);
+    mSharedPrefDataProvider = new SharedPrefDataProvider(this);
+    mFileDataProvider = new FileDataProvider(this);
   }
 }
